c0b3412b173355cdef6280b4b36c0498095c871b
Shantanu Karkare <shantanukarkare1706@gmail.com>
2025-08-05 18:13:51 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -10,14 +10,14 @@ def menu():
 def add_contact():
     name=input("Enter Contact Name :-").lower()
     mobile_no=input("Enter Mobile Number:-")
-    if not mobile_no.isdigit():
-        print("\"Please Enter a valid mobile number\"\n")
+    if not mobile_no.isdigit() or len(mobile_no)!=10:
+        print("\"Please Enter a valid 10 digit mobile number\"\n")
         return
     email=input("Enter your Email:-")
     if "@gmail.com" not in email:
         print("Please enter a valid email\n")
         return
-    address=input("Enter the Adress:-")
+    address=input("Enter the Address:-")
     contacts[name.lower()]={
         "Moblie number":mobile_no,
         "Email":email,
@@ -32,7 +32,7 @@ def view_contact():
     for name, i in contacts.items():
         print(f"Name:{name.title()}")
         for key, value in i.items():
-          print(f"{key}:{value}\n")
+          print(f"{key}:{value}")
 
 def search_contact():
     name=input("Enter Name of contact:").lower()
@@ -47,14 +47,19 @@ def update_contact():
    name=input("Enter contact name to update:").lower()
    if name in contacts:
     mobile_no=input("Enter updated Mobile Number:-")
+    if not mobile_no.isdigit() or len(mobile_no)!=10:
+        print("Please Enter a valid 10 digit mobile number\n")
+        return
     email=input("Enter your updated Email:-")
-    address=input("Enter updated adress:-")  
+    address=input("Enter updated address:-")  
     contacts[name.lower()]={
         "Mobile Number":mobile_no,
         "Email":email,
         "Address":address
     } 
-
+    print(f"Contact details of {name.title()} is updated successfully")   
+   else:
+       print("No contact found\n")
 def delete_contact():
     print("Enter the name of contact you want to delete?")
     name=input("Enter contact name:").lower()
@@ -62,26 +67,31 @@ def delete_contact():
       confirm=input(f"are you sure you want to delete {name.title()} (yes/no) ?").lower() 
       if confirm=="yes":
          del contacts[name]
-         print("Contact deleted Successfully \n!")   
+         print("Contact deleted Successfully \n!") 
+    else:
+        print("No contact found\n")
  
  #  Main body  
 while True:
     print("=====Welcome=====")
     menu()
-    choice=int(input("Enter the choice from(1-6):-"))
-    if choice==1:
-        add_contact()
-    elif choice==2:
-        view_contact()
-    elif choice==3:
-        search_contact()
-    elif choice==4:
-        update_contact()
-    elif choice==5:
-        delete_contact()
-    elif choice==6:
-        print("Exiting")
-        break
+    choice = int(input("Enter the choice from (1-6): "))
+    if choice == 1:
+            add_contact()
+    elif choice == 2:
+            view_contact()
+    elif choice == 3:
+            search_contact()
+    elif choice == 4:
+            update_contact()
+    elif choice == 5:
+            delete_contact()
+    elif choice == 6:
+            print("Exiting Contact Book. Goodbye!")
+            break
+    else:
+        print("Please enter a valid choice")
     
+
 
 
